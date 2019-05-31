e45427466d83f9ee41b3f65b9e9e9be55271b876
Thomas Johnson <tjohnson1106@gmail.com>
2019-05-31 15:18:09 -0400

    created redirect and added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 
 import { SyncingEditor } from "./components/SyncingEditor";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { GroupEditor } from "./components/GroupEditor";
 
 const App = () => {
   return (
-    <div>
-      <SyncingEditor />
-    </div>
+    <Router>
+      <Route
+        path="/"
+        exact
+        render={() => {
+          return <Redirect to={`/group/${Date.now()}`} />;
+        }}
+      />
+      <Route path="/group/:id" component={GroupEditor} />
+    </Router>
   );
 };
 
---
+++ src/components/GroupEditor.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+interface Props {}
+
+export const GroupEditor: React.FC<Props> = () => {
+  return <div>Group Editor</div>;
+};
